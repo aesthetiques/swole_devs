c162a63dd3c74ae324cb0a5b72892c6a4f7dac5e
Scott Brenden <scott.b.brenden@gmail.com>
2017-02-27 12:23:26 -0800

    added id numbers and filled in the beginner build five day split

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function newElement(elType, elAttribute, elAttributeName, elParentId, elText){
   parentEl.appendChild(el);
 }
 
-function Workout(sun, mon,tues, weds, thurs, fri, sat){
+function Workout(sun, mon,tues, weds, thurs, fri, sat,lvl,goal,split){
   this.sun = sun,
   this.mon = mon,
   this.tues = tues,
@@ -16,7 +16,10 @@ function Workout(sun, mon,tues, weds, thurs, fri, sat){
   this.thurs = thurs,
   this.fri = fri,
   this.sat = sat,
-  this.days = [sun,mon,tues,weds,thurs,fri,sat];
+  this.days = [sun,mon,tues,weds,thurs,fri,sat],
+  this.lvl = lvl,
+  this.goal = goal,
+  this.split = split;
 }
 
 Workout.prototype.printWorkout = function(){
@@ -30,7 +33,16 @@ var beginBuildThree = new Workout('Rest',
 'Rest',
 ['Squats 3x10','Overhead Press 3x10','Deadlift 3x10','Crunches 3,10'],
 'Rest',
-['Squats 3x10','Bench 3x10', 'Barbell Rows 3x10', 'Crunches 4x25'],'Rest');
-var beginBuild5 = new Workout('Rest',[],[],[],[],[],'Rest');
+['Squats 3x10','Bench 3x10', 'Barbell Rows 3x10', 'Crunches 4x25'],
+'Rest',
+0,1,0);
+var beginBuildFive = new Workout('Rest',
+['Squat 3x10','Leg press 3x10','Leg curls 3x10','Crunches 4x25'],
+['Dumbbell Bench Press 3x10','Dumbbell Flys 3x10','Incline Bench Press 3x10'],
+['Barbell Rows 3x10','Pullups 5x5','Widegrip Pulldowns 3x10','Crunches 4x25'],
+['Overhead Press 3x10','Front Dumbbell Raise 3x10','Dips 4x10'],
+['Barbell Curls 3x10','Tricep Extension 3x10','Crunches 3x10'],
+'Rest',
+0,1,1);
 
 beginBuildThree.printWorkout();
